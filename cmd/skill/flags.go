ed7aa641824d089bbb85401f6748c0d444ecc125
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-04-08 08:13:53 +0500

    env run addr

--- cmd/skill/flags.go
+++ cmd/skill/flags.go
@@ -1,10 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var flagRunAddr string
 
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "a", ":8080", "Address and port for server")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
 }
